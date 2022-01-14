9d26e550000031f45e54384352e709324a9fef94
Aniket <aniketdhokane2071@gmail.com>
2022-01-14 14:48:29 +0530

    CRUD with mongoose

--- index.js
+++ index.js
@@ -1,19 +1,40 @@
 
 const mongoose = require('mongoose')
 
-const main=async()=>{
+const saveInDB=async()=>{
     await mongoose.connect("mongodb://localhost:27017/e-comm");
 
     const ProductSchema=new mongoose.Schema({
         name:String,
-        price:Number
+        price:Number,
+        brand:String,
+        category:String
     });
 
     const ProductsModel= mongoose.model('products',ProductSchema);
-    let data=new ProductsModel({name:"m8",price:1000});
+    let data=new ProductsModel({name:"7T",price:38000,brand:"OnePlus",category:"Mobile"});
     let result= await data.save();
     console.log(result);
 
 }
 
-main()
+const updateInDB=async()=>{
+    const Product =mongoose.model('products',ProductSchema);
+    let data=await Product.updateOne(
+        {name:"7T"},
+        {
+            $set:{price:700}
+        }
+    )
+    console.log(data);
+
+
+}
+
+const deleteInDB=async()=>{
+    const Product =mongoose.model('products',ProductSchema);
+    let data= await Product.deleteOne({name:"m8"});
+    console.log(data);
+}
+
+deleteInDB()
